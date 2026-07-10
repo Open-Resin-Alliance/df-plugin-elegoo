edfe3cae80cb7c1a2e212e3e123b2bcabde97b32
SinXIV <sinxiv@proton.me>
2026-07-10 00:59:07 +0000

    correct which lift height gets subtracted from

--- slicing/rust/goo_v5.rs
+++ slicing/rust/goo_v5.rs
@@ -198,7 +198,7 @@ pub(super) fn build_goo_v5_container_bytes_with_progress(
     let mut out =
         Vec::with_capacity(end_of_rle as usize + GOO_V5_FOOTER_BLOB_SIZE + 32);
 
-    if timing.bottom_retract_distance2_mm <= job.layer_height_mm && timing.retract_distance2_mm <= job.layer_height_mm {
+    if timing.bottom_retract_distance2_mm >= job.layer_height_mm && timing.retract_distance2_mm >= job.layer_height_mm {
         timing.bottom_retract_distance2_mm -= job.layer_height_mm;
         timing.retract_distance2_mm -= job.layer_height_mm;
     } else {
